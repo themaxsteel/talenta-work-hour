cbe89a85fa5184c83d9d5b4288595d26b13d19b8
Turbo <piuresangkale@gmail.com>
2023-10-21 12:50:25 +0800

    adding summary attendance

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:talenta_work_hour/talenta_model.dart';
 
 class HomePage extends StatefulWidget {
@@ -21,20 +22,20 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Talenta"),
       ),
-      body: Padding(
+      body: ListView(
         padding: const EdgeInsets.all(32),
-        child: Column(
-          children: [
-            TextField(
-              controller: controller,
-              maxLines: 12,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: "Input Summary Attendance JSON",
-              ),
+        children: [
+          TextField(
+            controller: controller,
+            maxLines: 12,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: "Input Summary Attendance JSON",
             ),
-            const SizedBox(height: 24),
-            ElevatedButton(
+          ),
+          const SizedBox(height: 24),
+          Center(
+            child: ElevatedButton(
               onPressed: () {
                 talenta = Talenta.fromJson(jsonDecode(controller.text));
                 if (talenta != null) {
@@ -44,10 +45,106 @@ class _HomePageState extends State<HomePage> {
               },
               child: const Text("Submit"),
             ),
-            const SizedBox(height: 64),
-            if (talenta != null) Text("Total Jam Kerja: ${_printDuration(workHour!)}"),
-          ],
-        ),
+          ),
+          const SizedBox(height: 64),
+          if (talenta != null) ...[
+            Center(child: Text("Total Jam Kerja: ${_printDuration(workHour!)}")),
+            Center(
+              child: Text("Jam Kerja Yang Dibutuhkan: ${_printDuration(_calculateRequiredWorkHour(talenta!.data!))}"),
+            ),
+            const SizedBox(height: 32),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(child: Text("Tanggal")),
+                Expanded(child: Text("Clock In")),
+                Expanded(child: Text("Break In")),
+                Expanded(child: Text("Break Out")),
+                Expanded(child: Text("Clock Out")),
+                Expanded(child: Text("Akumulasi Jam Kerja")),
+              ],
+            ),
+            const SizedBox(height: 32),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: talenta!.data!.length,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                Attributes attributes = talenta!.data![index].attributes!;
+                Duration clockHour = const Duration();
+                Duration clock = const Duration();
+                Duration breaks = const Duration();
+
+                clock = dateParse(attributes.clockoutTime ?? attributes.clockOut ?? "2023-01-01")
+                    .difference(dateParse(attributes.clockinTime ?? attributes.clockIn ?? "2023-01-01"));
+
+                if (attributes.breakCheckin != null && attributes.breakCheckout != null) {
+                  breaks = dateParse(attributes.breakCheckout!).difference(dateParse(attributes.breakCheckin!));
+                }
+
+                clockHour += clock + breaks;
+
+                return Container(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: BoxDecoration(border: Border.all(color: const Color(0xFFEEEEEE))),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(child: Text(talenta!.data![index].attributes!.scheduleDate!)),
+                      Expanded(
+                        child: Text(
+                          DateFormat("HH:mm").format(
+                            DateTime.parse(
+                              talenta!.data![index].attributes!.clockIn ??
+                                  talenta!.data![index].attributes!.clockinTime ??
+                                  "2023-01-01",
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          DateFormat("HH:mm").format(
+                            DateTime.parse(
+                              talenta!.data![index].attributes!.breakCheckout ?? "2023-01-01",
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          DateFormat("HH:mm").format(
+                            DateTime.parse(
+                              talenta!.data![index].attributes!.breakCheckin ?? "2023-01-01",
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          DateFormat("HH:mm").format(
+                            DateTime.parse(
+                              talenta!.data![index].attributes!.clockOut ??
+                                  talenta!.data![index].attributes!.clockoutTime ??
+                                  "2023-01-01",
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          clockHour.isNegative ? "00 : 00 : 00" : _printDuration(clockHour),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: clockHour.inHours >= 1 ? Colors.green : Colors.red),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ]
+        ],
       ),
     );
   }
@@ -66,12 +163,30 @@ class _HomePageState extends State<HomePage> {
           clock = dateParse(attributes.clockOut!).difference(dateParse(attributes.clockIn!));
         }
 
+        // if (attributes.clockinTime != null &&
+        //     attributes.clockoutTime != null &&
+        //     attributes.clockIn != null &&
+        //     attributes.clockOut != null) {
+        //   clock = dateParse(attributes.clockoutTime ?? attributes.clockOut!)
+        //       .difference(dateParse(attributes.clockinTime ?? attributes.clockIn!));
+        // }
+
         Duration breaks = const Duration();
         if (attributes.breakCheckin != null && attributes.breakCheckout != null) {
           breaks = dateParse(attributes.breakCheckout!).difference(dateParse(attributes.breakCheckin!));
         }
         print("${attributes.scheduleDate} ${clock + breaks}");
         duration += (clock + breaks);
+      }
+    }
+    return duration;
+  }
+
+  Duration _calculateRequiredWorkHour(List<Data> data) {
+    Duration duration = Duration();
+    for (var item in data) {
+      if (item.attributes!.officeHourName != "dayoff") {
+        duration += const Duration(hours: 8);
       }
     }
     return duration;
